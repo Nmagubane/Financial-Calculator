9fcd568552959f2dfe6fe1669fac25d587a65a8c
nmaguban <nmaguban@student.wethinkcode.co.za>
2021-03-08 14:13:21 +0200

    Commit

--- FutureInvestmentValue/src/FutureInvestmentValue.java
+++ FutureInvestmentValue/src/FutureInvestmentValue.java
@@ -13,6 +13,16 @@ public class FutureInvestmentValue {
         System.out.println("Please enter the period in years: ");
         double period = sc.nextInt();
 
+	/* Here we calculate the future value using the formula:
+		FV = PV(1+r)pow.n
+		
+		FV is the Future Value (futureValue)
+		PV is the Present Value (present)
+		r is the interest rate (interest)
+			- We divide the interest rate by 100, to convert it
+			  from percentage to integer
+		n is the period (period). i.e the period of the investment
+	*/
         double futureValue = present * Math.pow((1 + interest/100), period);
         System.out.println("Future Value of the asset is: " + futureValue);
 
